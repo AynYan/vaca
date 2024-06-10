994c158ef4cb0830ceffc0e1149347eed6725577
AynYan <aynmathdude@gmail.com>
2024-06-10 11:41:14 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,37 +4,37 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chains import SequentialChain
 
-def generate_people_names(gender: str,nationality:str) -> list[str]:
+def generate_boosk_to_read(favorite_book: str,favorite_book_series:str) -> list[str]:
     """
     Generate a list of 5 baby names
 
     Parameters:
-    age (str): age of person
-    nationailty (str) : nationailty of person
+    favorite_book (str): favorite singular book
+    favorite_book_series (str) : 1st favorite book series
 
     Returns:
-    list: list of peoples' names
+    list: list of books to read
     """
 
     prompt_template_name = PromptTemplate(
-        input_variables=['gender', 'nationality'],
-        template="""I want to find a name for a {nationality} {age} person. 
-                    Suggest top 3 popular names for the baby.
-                    Return it as a comma separated list """
+        input_variables=['gender', 'favorite_book'],
+        template="""= I like the book series {favorite_book_series} and the book called: {favorite_book}. 
+        Suggest the top 5 books I should read based on my favorite books.
+                     """
                 )
 
     name_chain = LLMChain(llm=llm,
                           prompt=prompt_template_name,
-                          output_key='people_names')
+                          output_key='books_to_read')
 
     chain = SequentialChain(
         chains=[name_chain],
-        input_variables=['age', 'nationality'],
-        output_variables=['people_names']
+        input_variables=['favorite_book_series', 'favorite_book'],
+        output_variables=['books_to_read']
     )
 
-    response = chain({'age': age,
-                      'nationality': nationality})
+    response = chain({'favorite_book_series': favorite_book_series,
+                      'favorite_book': favorite_book})
     return response
 
 
@@ -55,20 +55,18 @@ import os
 os.environ['OPENAI_API_KEY'] = openai_api_key
 llm = OpenAI(model_name="gpt-3.5-turbo-instruct", temperature = 0.6)
 
-# DO NOT CHANEGE ABOVE ----
+# DO NOT CHANGE ABOVE ----
 
 
 # ask user for what they want
-age = st.selectbox("Choose an age group",
-                             ("0-12","13-21","22-44","45-65","66-89","90-110"))
-nationality = st.selectbox("Choose the nationality", 
-                                  ("American", "Asian","European","African","South American"))
+favorite_book_series = st.text_input("Enter the name of your favorite book series: ")
+favorite_book = st.text_input("Enter the name of your favorite book: ")
 
 # get the answer from LLM
-if age and nationality:
-    response = generate_people_names(age, nationality)
-    people_names = response['people_names'].strip().split(",")
-    st.write("** Top 3 Baby Names **")
+if favorite_book_series and favorite_book:
+    response = generate_books_to_read(favorite_book_series, favorite_book)
+    people_names = response['books_to_read'].strip().split(",")
+    st.write("** Top 5 Books to Read **")
 
-    for name in people_names:
-        st.write("--", name)
+    for book in books_to_read:
+        st.write("--", book)
