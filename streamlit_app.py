c3df2110a5caaf568213167d720d3e6cd04bef71
AynYan <aynmathdude@gmail.com>
2024-06-29 18:13:36 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,7 +80,9 @@ llm = OpenAI(model_name="gpt-3.5-turbo-instruct", temperature = 0.6)
 
 
 # ask user for what they want
-q1 = st.text_input("What kind of climate do you prefer for your vacation?: ")
+#q1 = st.text_input("What kind of climate do you prefer for your vacation?: ")
+q1 = st.chat_input("What kind of climate do you prefer for your vacation?")
+
 q2 = st.text_input("Are you looking for a beach destination, a city with a warm climate, or a mix of both?: ")
 q3 = st.text_input(" Do you prefer a vacation spot that is more relaxed and laid-back or one that offers a lot of activities and entertainment?: ")
 q4 = st.selectbox("Are you interested in exploring cultural and historical sites during your vacation?: ",["Pick an option","Yes","No","Maybe"])
