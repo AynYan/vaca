26cc3f4f73d3662926b0cb6e9154b4ab45285587
AynYan <aynmathdude@gmail.com>
2024-06-29 19:31:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,18 +85,23 @@ ph = st.empty()
 q1 = ph.text_input("What kind of climate do you prefer for your vacation?: ")
 if q1:
     ph.empty()
-    q2 = st.text_input("Are you looking for a beach destination, a city with a warm climate, or a mix of both?: ")
+    q2 = ph.text_input("Are you looking for a beach destination, a city with a warm climate, or a mix of both?: ")
+    ph.empty()
     if q2:
-        q3 = st.text_input(" Do you prefer a vacation spot that is more relaxed and laid-back or one that offers a lot of activities and entertainment?: ")
+        q3 = ph.text_input(" Do you prefer a vacation spot that is more relaxed and laid-back or one that offers a lot of activities and entertainment?: ")
+        ph.empty
         if q3:    
-            q4 = st.selectbox("Are you interested in exploring cultural and historical sites during your vacation?: ",["Pick an option","Yes","No","Maybe"])
+            q4 = ph.selectbox("Are you interested in exploring cultural and historical sites during your vacation?: ",["Pick an option","Yes","No","Maybe"])
+            ph.empty()
             if q4:        
-                q5 = st.text_input("Do you prefer traveling within your own country or are you open to international destinations?: ")
+                q5 = ph.text_input("Do you prefer traveling within your own country or are you open to international destinations?: ")
+                ph.empty()
                 if q5:
-                    q6 = st.selectbox("Do you have a preference for a certain type of accommodation, such as a resort, hotel, vacation rental, or something else?: ",["Pick an option","I dont care","Hotel","Resort","Vacation Rental","Other"])
+                    q6 = ph.selectbox("Do you have a preference for a certain type of accommodation, such as a resort, hotel, vacation rental, or something else?: ",["Pick an option","I dont care","Hotel","Resort","Vacation Rental","Other"])
+                    ph.empty()
                     if q6:
-                        q7 = st.text_input("Are you interested in a particular region of the USA, such as the East Coast, West Coast, South, or somewhere else?: ")
-
+                        q7 = ph.text_input("Are you interested in a particular region of the USA, such as the East Coast, West Coast, South, or somewhere else?: ")
+                        ph.empty
 #get the answer from LLM
 if q1 and q2 and q3 and q4 and q5 and q6 and q7:
     response = generate_the_vaca_vaca(q1, q2,q3,q4,q5,q6,q7)
